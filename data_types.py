c593e047c2fc98876fbe962cebfb92e3461edfdc
mauro <mauro_biazutti@hotmail.com>
2023-11-11 16:25:37 -0300

    data tipes

---
+++ data_types.py
@@ -0,0 +1,45 @@
+#Inteiros
+x = 1
+y = 1000
+z = -10
+
+#Ponto flutuante Float
+a = 1.2
+b = 3.4
+c = -5.6
+
+#Complexos
+d = 1 + 2i
+e = 3 - 4i
+f = 0.5i
+
+#Strings
+ola = "Olá, mundo!"
+sequencia = "1234567890"
+caracteres = "!"
+
+#Booleanos
+verdadeiro = True
+falso = False
+truee = (1 > 0)
+
+#Listas
+m = [1, 2, 3]
+n = ["a", "b", "c"]
+o = [True, False, True]
+
+#Tuplas
+p = (1, 2, 3)
+q = ("a", "b", "c")
+r = (True, False, True)
+
+#Dicionários
+chave_valor = {"nome": "Olá, mundo!", "idade": 30}
+t = {"a": 1, "b": 2, "c": 3}
+
+
+#set (conjunto):
+conjunto = {1, 2, 3}
+
+#Tipos de None:
+nulo = None
--- variables.py
+++ variables.py
@@ -9,3 +9,15 @@ a, b, c = 1, 2, 3
 
 # Exemplo 3: Variáveis podem ser utilizadas em expressões
 soma = a + b + c
+
+# Exemplo de constante (convenção)
+TAXA_DE_JUROS = 0.05
+DIAS_DA_SEMANA = 7
+
+# Tentativa de modificar a constante (gerará um aviso, mas não impedirá a modificação)
+TAXA_DE_JUROS = 0.1  # Embora isso seja possível, é uma prática ruim
+
+# Outra abordagem (usando uma tupla para imitar a imutabilidade)
+constantes = (0.05, 7)
+TAXA_DE_JUROS = constantes[0]  # Isso é mais seguro, mas ainda não é completamente imutável
+
