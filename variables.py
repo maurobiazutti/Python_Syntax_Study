11a7c78597832b5f3bae2f738dfa168d73235b2d
maurobiazutti <mauro_biazutti@hotmail.com>
2023-11-11 18:28:20 -0300

    variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Exemplo 1: Atribuindo um valor a uma variável
+nome = "João"
+idade = 25
+altura = 1.75
+sobre_nome = "Vilela"
+
+# Exemplo 2: Python permite múltiplas atribuições em uma única linha
+a, b, c = 1, 2, 3
+
+# Exemplo 3: Variáveis podem ser utilizadas em expressões
+soma = a + b + c
