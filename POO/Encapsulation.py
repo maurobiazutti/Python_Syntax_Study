276b115012e77f167aaf061849b4710e6359f93b
mauro <mauro_biazutti@hotmail.com>
2023-11-20 19:22:46 -0300

    emcapsulation

--- POO/Encapsulation.py
+++ POO/Encapsulation.py
@@ -1,8 +1,12 @@
+#Ecapssulamento esta ligado a permições de uso.
 # public -> Podem ser acessados dentro e fora da class
 # protected -> Podem ser acessados somente dentro da class ou filhas da class
 # _protected 
 # private -> Pode ser usada somente dentro da class
 # __private (_NomeDaClass__nomeatributo) para acessar e modificar
+
+from typing import Any
+
 
 class BaseDeDados:
     def __init__(self):
@@ -34,3 +38,38 @@ db.inserir_cliente(3, 'Marcos')
 
 db.apaga_cliente(2)
 db.lista_clientes()
+
+print('##################################################################')
+
+class Pessoa:
+    
+    def __init__(self, nome, idade, cpf):
+        self.nome = nome
+        self.idade = idade
+        self.__cpf = cpf # Atributo privado
+        
+    def print_cpf(self):
+        print(self.__cpf)
+    
+    def beber(self, bebida):
+        if bebida == 'cerveja':
+            self.__apresentar_documento()
+        print('bebendo...')
+     
+     # Medoto Privado       
+    def __apresentar_documento(self): 
+        print(self.__cpf)
+        
+        
+        
+marcos = Pessoa('Marcos', 32, '123456123')
+print(marcos.nome)
+print(marcos.idade)
+#print(marcos.__cpf) # Atributo privado não pode ser acessado
+marcos.print_cpf()
+print('##################################################################')
+
+marcos.beber('cerveja')
+
+print('##################################################################')
+marcos.beber('coca')
